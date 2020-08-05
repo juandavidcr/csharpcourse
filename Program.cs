6645ed048bd67d54ab80c6d2a201a05d9d74c599
Juan David Contreras <j1dcrane@gmail.com>
2020-08-05 17:57:10 -0500

    'csharp'

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HOLAMUNDO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            const double valorPI = 3.1416;
+            Console.WriteLine("Programa que te calcula el área de un cículo dándole el radio");
+
+            Console.WriteLine("Introduce tu radio por favor");
+            double num1= double.Parse(Console.ReadLine());
+            var resultado = valorPI*(num1*num1);
+            Console.WriteLine("resultado: " + resultado);
+
+        }
+    }
+}
